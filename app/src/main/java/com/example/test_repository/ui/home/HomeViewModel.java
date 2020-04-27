efbd46067440292f7981cdede5e9f78dbb680ff3
Oscar-x <2796769363@qq.com>
2020-04-27 19:55:50 +0800

    改了 home

--- app/src/main/java/com/example/test_repository/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/test_repository/ui/home/HomeViewModel.java
@@ -1,10 +1,33 @@
 package com.example.test_repository.ui.home;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.test_repository.databinding.FragmentHomeBinding;
+
 public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<Integer> num;
+
+
+
+
+    public MutableLiveData<Integer> getNum() {
+        if(num==null){
+            num=new MutableLiveData<>();
+            num.setValue(0);
+        }
+        return num;
+    }
+    public void add(){
+        num.setValue(num.getValue()+1);
+    }
 
     private MutableLiveData<String> mText;
 
