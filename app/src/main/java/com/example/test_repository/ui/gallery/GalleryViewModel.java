1ddb9156c88d9149d2a6a50be9c463f2f4f0fa72
soreak <1498985670@qq.com>
2020-04-27 18:23:30 +0800

    create +1 btn

--- app/src/main/java/com/example/test_repository/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/test_repository/ui/gallery/GalleryViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel;
 public class GalleryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<Integer> Num;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
@@ -14,6 +15,22 @@ public class GalleryViewModel extends ViewModel {
     }
 
     public LiveData<String> getText() {
+        if (mText == null) {
+            mText = new MutableLiveData<>();
+            mText.setValue(" ");
+        }
         return mText;
+    }
+
+    public MutableLiveData<Integer> getNum() {
+        if (Num == null) {
+            Num = new MutableLiveData<>();
+            Num.setValue(0);
+        }
+        return Num;
+    }
+
+    public void addNum() {
+        Num.setValue(Num.getValue() + 1);
     }
 }
